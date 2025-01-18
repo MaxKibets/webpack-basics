382767c0aa75ff84e010bdc124df8549731978af
MaxKibets <kibetss3@gmail.com>
2025-01-18 11:46:41 +0200

    configure asset module to import images

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"), // output directory. Should be absolute path!
   },
   mode: "none", // mode of bundling
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/, // file types to be handled
+        type: 'asset/resource', // type of handling
+      },
+    ],
+  },
 };
