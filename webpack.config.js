4bb33411078f9604373b650bf5a19a6bbb0e1853
MaxKibets <kibetss3@gmail.com>
2025-01-18 12:16:37 +0200

    set asset moduls types

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,24 @@ module.exports = {
   output: {
     filename: "bundle.js", // output file name
     path: path.resolve(__dirname, "./dist"), // output directory. Should be absolute path!
+    // Path to the output directory.
+    // It can be usefull for CDN (example: publicPath: "https://cdn.example.com/")
+    publicPath: "dist/",
   },
   mode: "none", // mode of bundling
   module: {
     rules: [
       {
         test: /\.(png|jpg)$/, // file types to be handled
-        type: 'asset/resource', // type of handling
+        // type: "asset/resource", // type of handling
+        // type: "asset/inline", // image will be inlined as base64
+        // webpack will automatically choose between inline (less 8kB) and resource (more 8kB)
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024, // 3kB (8kB by default)
+          },
+        },
       },
     ],
   },
